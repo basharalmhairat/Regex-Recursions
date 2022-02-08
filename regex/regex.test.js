c401f23b6873897ceaabd1ccae1ad8a7729711cb
basharalmhairat <basharalmhairat6y6u@gmail.com>
2022-02-08 19:06:00 +0200

    solved

--- regex/regex.test.js
+++ regex/regex.test.js
@@ -1,38 +1,39 @@
 'use strict';
-
-
-
 /* Write a function that take a string and return true if the string only contain uppercase and lowercase
 characters (no numbers and symbols) and it should end with capital A else return false */
 
 function capitalA(s){
-    // Add your logic.
-    return;
-}
-
-
+    var pattern = new RegExp( "^(?=.*[a-z])+(?=.*[A-Z])+(?=.*[A$])" );
+      if (pattern.test(s)) {
+       return(true);
+      } else {
+        return(false);
+      }
+    }
 /* Write a function that take a string and return true if the the sting is following the emails pattern
 which end with io (example@example.io) */
-
 function ioEmail(email){
-    // Add your logic.
-    return;
+   var pattern = new RegExp ("^[^@.\s]+@[^@\s]+\.[io$]+$" );
+   if (pattern.test(email)) {
+    return(true);
+   } else {
+     return(false);
+   }
 }
-
 /* You have a text that contain image names with their extention you need to write a function to 
 find all images in that text and return their names and extention in an array 
 
 required extention are jpg, jpeg and png.
-
 */
-
 function imagesSearcher(text){
     let arr = [];
-    // Add your logic.
-    return arr
+    let pattern = new RegExp (  "/[a-z]+.(jpg|png|txt)/g"  );
+    let result =  text.match(RegExp);
+   
+   if (pattern.test(text)) { arr.push(result)} ;
+   
+    return arr;
 }
-
-
 describe("Test capitalA", ()=>{
     test("It should return true if the input has uppercase and lowercase characters (no numbers and symbols) and it should end with capital A else return false ", () => {
         expect(capitalA("Hello world A")).toStrictEqual(true);
